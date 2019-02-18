0e40c899f1fccd62b0b1fb13cec4a19080793064
Seyi Onifade <xyluz@ymail.com>
2019-02-18 09:13:29 +0100

    moving project to the cloud

---
+++ JS/script.js
@@ -0,0 +1,16 @@
+//js file
+
+// keyword -- function, 
+// name of the function
+// and parenthesis (brackets)
+// add curly braces to indicate block of js code.
+
+function theEvent(){
+
+	//display the element
+	//insert message into the element
+	document.getElementById('message-alert').style = "display:block";
+
+	document.getElementById('message-alert').innerHTML = "I am a dangerous alert message!";
+
+}
